008c6eaa66d035261528b2ccf03d60b13093fae5
Mostafa Moradian <mstfmoradian@gmail.com>
2023-12-12 23:18:24 +0100

    Password authentication works

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -5,7 +5,6 @@ import (
 	"encoding/base64"
 
 	"github.com/gatewayd-io/gatewayd-plugin-sdk/databases/postgres"
-	sdkPlugin "github.com/gatewayd-io/gatewayd-plugin-sdk/plugin"
 	v1 "github.com/gatewayd-io/gatewayd-plugin-sdk/plugin/v1"
 	"github.com/hashicorp/go-hclog"
 	goplugin "github.com/hashicorp/go-plugin"
@@ -13,6 +12,12 @@ import (
 	"github.com/spf13/cast"
 	"google.golang.org/grpc"
 )
+
+var errorResponse = pgproto3.ErrorResponse{
+	Severity: "ERROR",
+	Code:     "28P01",
+	Message:  "password authentication failed",
+}
 
 type Plugin struct {
 	goplugin.GRPCPlugin
@@ -64,66 +69,75 @@ func (p *Plugin) OnTrafficFromClient(ctx context.Context, req *v1.Struct) (*v1.S
 		p.Logger.Info("Failed to handle client message", "error", err)
 	}
 
-	if val := req.Fields["startupMessage"]; val != nil {
-		startupMessageEncoded := cast.ToString(sdkPlugin.GetAttr(req, "startupMessage", ""))
-		if startupMessageEncoded != "" {
-			startupMessageDecoded, err := base64.StdEncoding.DecodeString(startupMessageEncoded)
-			if err != nil {
-				p.Logger.Info("Failed to decode startup message", "error", err)
-				return nil, err
-			}
-
-			startupMessage := cast.ToStringMapString(string(startupMessageDecoded))
-			p.Logger.Info("OnTrafficFromClient", "startupMessage", startupMessage)
-
-			if startupMessage["Type"] == "StartupMessage" {
-				p.Logger.Info("OnTrafficFromClient", "User is correct")
-
-				response := pgproto3.AuthenticationCleartextPassword{}
-				req.Fields["response"] = v1.NewBytesValue(response.Encode(nil))
-				req.Fields["terminate"] = v1.NewBoolValue(true)
-			} else {
-				p.Logger.Info("OnTrafficFromClient", "User is incorrect")
-
-				response := pgproto3.ErrorResponse{
-					Message: "User is incorrect",
-				}
-				req.Fields["response"] = v1.NewBytesValue(response.Encode(nil))
-				req.Fields["terminate"] = v1.NewBoolValue(true)
-			}
+	if val, exists := req.Fields["startupMessage"]; exists {
+		startupMessageDecoded, err := base64.StdEncoding.DecodeString(val.GetStringValue())
+		if err != nil {
+			p.Logger.Info("Failed to decode startup message", "error", err)
+			return nil, err
 		}
-	} else if val := req.Fields["passwordMessage"]; val != nil {
-		passwordMessageEncoded := cast.ToString(sdkPlugin.GetAttr(req, "passwordMessage", ""))
-		if passwordMessageEncoded != "" {
-			passwordMessageDecoded, err := base64.StdEncoding.DecodeString(passwordMessageEncoded)
-			if err != nil {
-				p.Logger.Info("Failed to decode password message", "error", err)
-				return nil, err
+
+		startupMessage := cast.ToStringMap(string(startupMessageDecoded))
+		parameters := cast.ToStringMapString(startupMessage["Parameters"])
+		p.Logger.Info("OnTrafficFromClient", "startupMessage", parameters)
+
+		if parameters["user"] == "postgres" {
+			p.Logger.Info("OnTrafficFromClient", "message", "User is correct")
+
+			response := pgproto3.AuthenticationCleartextPassword{}
+			req.Fields["response"] = v1.NewBytesValue(response.Encode(nil))
+			req.Fields["terminate"] = v1.NewBoolValue(true)
+		} else {
+			p.Logger.Info("OnTrafficFromClient", "message", "User is incorrect")
+
+			req.Fields["response"] = v1.NewBytesValue(errorResponse.Encode(nil))
+			req.Fields["terminate"] = v1.NewBoolValue(true)
+		}
+	} else if val, exists := req.Fields["passwordMessage"]; exists {
+		passwordMessageDecoded, err := base64.StdEncoding.DecodeString(val.GetStringValue())
+		if err != nil {
+			p.Logger.Info("Failed to decode password message", "error", err)
+			return nil, err
+		}
+
+		passwordMessage := cast.ToStringMapString(string(passwordMessageDecoded))
+		p.Logger.Info("OnTrafficFromClient", "passwordMessage", passwordMessage)
+
+		if passwordMessage["Password"] == "postgres" {
+			p.Logger.Info("OnTrafficFromClient", "message", "Password is correct")
+
+			authOK := pgproto3.AuthenticationOk{}
+			pStat1 := pgproto3.ParameterStatus{
+				Name:  "client_encoding",
+				Value: "UTF8",
 			}
-
-			passwordMessage := cast.ToStringMapString(string(passwordMessageDecoded))
-			p.Logger.Info("OnTrafficFromClient", "passwordMessage", passwordMessage)
-
-			if passwordMessage["Password"] == "postgres" {
-				p.Logger.Info("OnTrafficFromClient", "Password is correct")
-
-				authOK := pgproto3.AuthenticationOk{}
-				readyForQuery := pgproto3.ReadyForQuery{TxStatus: 'I'}
-				response := readyForQuery.Encode(authOK.Encode(nil))
-				req.Fields["response"] = v1.NewBytesValue(response)
-				req.Fields["terminate"] = v1.NewBoolValue(true)
-			} else {
-				p.Logger.Info("OnTrafficFromClient", "Password is incorrect")
-
-				response := pgproto3.ErrorResponse{
-					Message: "Password is incorrect",
-				}
-				req.Fields["response"] = v1.NewBytesValue(response.Encode(nil))
-				req.Fields["terminate"] = v1.NewBoolValue(true)
+			pStat2 := pgproto3.ParameterStatus{
+				Name:  "server_version",
+				Value: "15.4",
 			}
+			backendKeyData := pgproto3.BackendKeyData{
+				ProcessID: 12345,
+				SecretKey: 54321,
+			}
+			readyForQuery := pgproto3.ReadyForQuery{TxStatus: 'I'}
+			response := readyForQuery.Encode(
+				backendKeyData.Encode(
+					pStat1.Encode(
+						pStat2.Encode(
+							authOK.Encode(nil),
+						),
+					),
+				),
+			)
+			req.Fields["response"] = v1.NewBytesValue(response)
+			req.Fields["terminate"] = v1.NewBoolValue(true)
+		} else {
+			p.Logger.Info("OnTrafficFromClient", "message", "Password is incorrect")
+
+			req.Fields["response"] = v1.NewBytesValue(errorResponse.Encode(nil))
+			req.Fields["terminate"] = v1.NewBoolValue(true)
 		}
 	} else {
-		p.Logger.Info("OnTrafficFromClient", "Regular message")
+		p.Logger.Info("OnTrafficFromClient", "message", "Regular message", "req", req)
 	}
 
 	return req, nil
